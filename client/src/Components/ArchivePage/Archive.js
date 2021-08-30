be1774149698e5eb3a477b1c591d1f2cf2d42c2d
YasmeenTak <yasmeen.tak.33@gmail.com>
2021-08-30 16:10:19 +0300

    archive

--- client/src/Components/ArchivePage/Archive.js
+++ client/src/Components/ArchivePage/Archive.js
@@ -116,34 +116,31 @@ export default function VerticalTabs() {
             />
           </div>
 
-          <Tab label='Item One' {...a11yProps(0)} />
-          <Tab label='Item Two' {...a11yProps(1)} />
-          <Tab label='Item Three' {...a11yProps(2)} />
-          <Tab label='Item Four' {...a11yProps(3)} />
-          <Tab label='Item Five' {...a11yProps(4)} />
-          <Tab label='Item Six' {...a11yProps(5)} />
-          <Tab label='Item Seven' {...a11yProps(6)} />
+          <Tab label='Item One' {...a11yProps(1)} />
+          <Tab label='Item Two' {...a11yProps(2)} />
+          <Tab label='Item Three' {...a11yProps(3)} />
+          <Tab label='Item Four' {...a11yProps(4)} />
+          <Tab label='Item Five' {...a11yProps(5)} />
+          <Tab label='Item Six' {...a11yProps(6)} />
         </Tabs>
-        <TabPanel value={value} index={0}>
-          Item One
-        </TabPanel>
+
         <TabPanel value={value} index={1}>
-          Item Two
+          Item one
         </TabPanel>
         <TabPanel value={value} index={2}>
-          Item Three
+          Item Two
         </TabPanel>
         <TabPanel value={value} index={3}>
-          Item Four
+          Item Three
         </TabPanel>
         <TabPanel value={value} index={4}>
-          Item Five
+          Item Four
         </TabPanel>
         <TabPanel value={value} index={5}>
-          Item Six
+          Item Five
         </TabPanel>
         <TabPanel value={value} index={6}>
-          Item Seven
+          Item Six
         </TabPanel>
       </div>
     </div>
